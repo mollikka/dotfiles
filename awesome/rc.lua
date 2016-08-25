a549661b7b68d8d46d82cec89d8f8b4f855e8741
Lauri Tervonen <lauri.tervonen@aalto.fi>
2016-08-25 11:11:45 +0300

    Awesome: use wicd graphics app for wlan connection

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -155,7 +155,7 @@ volumewidget:buttons(awful.button({ }, 1, function() awful.util.spawn(terminal .
 
 wifiwidget = wibox.widget.textbox()
 vicious.register(wifiwidget, vicious.widgets.wifi, "  WLAN: ${ssid} ${linp}%  ",10 , "wlp2s0");
-wifiwidget:buttons(awful.button({ }, 1, function() awful.util.spawn(terminal .. " -x wicd-curses") end))
+wifiwidget:buttons(awful.button({ }, 1, function() awful.util.spawn(internet) end))
 
 updatewidget = wibox.widget.textbox()
 vicious.register(updatewidget, vicious.widgets.pkg,
