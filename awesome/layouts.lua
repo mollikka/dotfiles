53a4cb281022c54e2f9d828cb24b3ff50e3010c1
Lauri Tervonen <lauri.tervonen@aalto.fi>
2017-04-05 14:11:39 +0300

    Awesome: fixed duallayout again
    let's hope it works this time
    !

--- awesome/layouts.lua
+++ awesome/layouts.lua
@@ -14,59 +14,30 @@ layouts.duallayout.arrange = function(p)
   local work_height = p.workarea.height
   local clients = p.clients
 
+  local left_pos = work_x
+  local right_pos = work_x + work_width/2
+  local hide_pos = work_x + work_width
 
-  -- Draw the first two clients. The rest will be outside visible screen.
-  -- I regret nothing
-  for i = 1,#clients
-  do
+  for i = 1,#clients do
+
     local c = clients[i]
     local g = {}
+    local in_focus = (c == client.focus)
 
     g.width = work_width/2
     g.height = work_height
 
-    left_pos = work_x
-    right_pos = work_x + work_width/2
-    hide_pos = work_x + work_width
-
-    one_in_focus = clients[1] == client.focus
-    two_in_focus = clients[2] == client.focus
-    n_in_focus = not(one_in_focus) and not(two_in_focus)
-
-    if (c == client.focus) then
-      if (i == 1) then
-        g.x = left_pos
-      else
-        g.x = right_pos
-      end
+    if (i == 1) then
+      g.x = left_pos
+    else if (i == 2) then
+      g.x = right_pos
+    else if (in_focus) then
+      g.x = right_pos
     else
-      if (one_in_focus and i==2) then
-        g.x = right_pos
-      end
-      if (one_in_focus and i>2) then
-        g.x = hide_pos
-      end
-      if (two_in_focus and i==1) then
-        g.x = left_pos
-      end
-      if (two_in_focus and i>2) then
-        g.x = hide_pos
-      end
-      if (n_in_focus and i==1) then
-        g.x = left_pos
-      end
-      if (n_in_focus and i==2) then
-        g.x = hide_pos
-      end
-      if (n_in_focus and i>2) then
-        g.x = hide_pos
-      end
-    end
-
+      g.x = hide_pos
+    end end end
     g.y = work_y
-
     c:geometry(g)
-
   end
 end
 
