5bcdc15e91183d2bcb47accc2f2e8b09c0753201
Lauri Tervonen <lauri.tervonen@aalto.fi>
2017-04-29 14:11:48 +0300

    Awesome: refactor terminal_execute command

--- awesome/definitions.lua
+++ awesome/definitions.lua
@@ -1,15 +1,16 @@
 local definitions = {}
 -- {{{ Variable definitions
 local terminal = "urxvt"
-local terminal_execute = "urxvt -e "
+local terminal_execute = function(cmd) return "urxvt -e " .. cmd end
 definitions.terminal = terminal
+definitions.terminal_execute = terminal_execute
 definitions.web = "chromium"
 definitions.webmini = "chromium --app=http://reddit.com"
 definitions.email = "thunderbird"
-definitions.internet = terminal_execute .. "sudo wifi-menu"
+definitions.internet = terminal_execute("sudo wifi-menu")
 definitions.shutdown = "shutdown now"
 definitions.restart = "shutdown now -r"
-definitions.soundsettings = terminal_execute .. "alsamixer"
+definitions.soundsettings = terminal_execute("alsamixer")
 definitions.lockscreen = "xscreensaver-command -lock"
 definitions.volume_up = "amixer set Master 5%+"
 definitions.volume_down = "amixer set Master 5%-"
