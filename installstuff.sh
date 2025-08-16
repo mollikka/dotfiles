c4fc2c9364f94d8a87008f156a84c64ad252575a
Lauri Tervonen <lauri.tervonen@iki.fi>
2025-08-16 13:01:56 +0300

    Dotfiles: update installstuff script
    Remove the distros that I haven't used in a while

--- installstuff.sh
+++ installstuff.sh
@@ -11,21 +11,9 @@ fi
 
 if [ -x "$(command -v pacman)" ]; then
   echo "Using pacman to install stuff"
-  sudo pacman -S zsh git vim openssh
+  sudo pacman -S zsh git gvim openssh
   if [ $1 == "visual" ]; then
-    sudo pacman -S bspwm sxhkd xscreensaver imagemagick chromium rxvt-unicode alsa-utils urxvt-perls compton
-  fi
-elif [ -x "$(command -v apt-get)" ]; then
-  echo "Using apt to install stuff"
-  sudo apt-get install vim zsh git
-  if [ $1 == "visual" ]; then
-    sudo apt-get install xscreensaver rxvt-unicode unclutter chromium-browser compton
-  fi
-elif [ -x "$(command -v zypper)" ]; then
-  echo "Using zypper to install stuff"
-  sudo zypper install zsh git vim
-  if [ $1 == "visual" ]; then
-    sudo zypper install unclutter xscreensaver ImageMagick chromium rxvt-unicode alsa-utils urxvt-perls compton
+    sudo pacman -S bspwm sxhkd rxvt-unicode alsa-utils urxvt-perls xfce4-panel
   fi
 else
   echo "I don't know how to install stuff for you :("
